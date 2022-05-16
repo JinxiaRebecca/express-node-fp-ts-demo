58a0e89299572feadbf00f7c8ffe945991896469
JinxiaRebecca <18502908857@163.com>
2022-05-16 14:38:06 +0800

    demo for pipe and flow

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,6 @@
 import express = require('express');
+import { number } from 'fp-ts';
+import { pipe, flow } from 'fp-ts/lib/function';
 
 const app : express.Application = express();
 
@@ -7,5 +9,18 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log('Demo app listenning on port 8080');
+    console.log('Demo app listenning on port 3000');
 });
+
+function add1(num: number): number {
+    return num + 1;
+}
+
+function multiply1(num: number): number {
+    return num * 2;
+}
+
+const pipeResult = pipe(1, add1, multiply1);
+const flowResult = flow(add1, multiply1)(1);
+console.log('pipe:' + pipeResult);
+console.log('flow:' + flowResult);
