6e9d306f3a446864068efe3a4d28dee24ccafe04
JinxiaRebecca <18502908857@163.com>
2022-05-16 16:03:49 +0800

    add demo for option

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
 import express = require('express');
 import { number } from 'fp-ts';
 import { pipe, flow } from 'fp-ts/lib/function';
+import * as O from 'fp-ts/Option';
+import { Json } from 'io-ts-types';
 
 const app : express.Application = express();
 
@@ -24,3 +26,29 @@ const pipeResult = pipe(1, add1, multiply1);
 const flowResult = flow(add1, multiply1)(1);
 console.log('pipe:' + pipeResult);
 console.log('flow:' + flowResult);
+
+const doubleIfEvenElseNone = (n: number) => n % 2 === 0
+  ? O.some(2 * n)
+  : O.none
+
+const addIfEven = (n: number) => n % 2 === 0
+  ? O.some(n + 1)
+  : O.some(n)
+
+  const optionEven = O.some(2);
+  const optionOdd = O.some(1);
+
+const even = pipe(
+  optionEven,
+  O.map(doubleIfEvenElseNone),
+  O.flatten
+);
+
+console.log("option even" + JSON.stringify(even));
+
+const odd = pipe(
+  optionOdd,
+  O.chain(doubleIfEvenElseNone),
+  O.chain(addIfEven)
+);
+console.log("option odd "+ JSON.stringify(odd))
