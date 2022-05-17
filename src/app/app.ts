5e938390b757d35b47a0cd2e761cbcf99f2806e2
JinxiaRebecca <18502908857@163.com>
2022-05-17 10:17:50 +0800

    add taskEither demo

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,11 @@ import express = require('express');
 import { pipe, flow } from 'fp-ts/lib/function';
 import * as O from 'fp-ts/Option';
 import * as E from 'fp-ts/lib/Either';
+import * as TE from 'fp-ts/lib/TaskEither';
+import * as T from 'fp-ts/lib/Task';
+import { string } from 'fp-ts';
+import { fromEither } from 'fp-ts/lib/OptionT';
+import { right } from 'fp-ts/lib/EitherT';
 
 const app : express.Application = express();
 
@@ -91,5 +96,36 @@ const testUser: UserInfo = {
   userName: "Anglela",
   password: "344555"
 }
-validateUserInfo(testUser)
-validateUserInfo(testUserInvalidName)
+validateUserInfo(testUser);
+validateUserInfo(testUserInvalidName);
+
+//TaskEither
+type Item = {
+  id: string,
+  name: string
+}
+
+type itemIdsByList = (listId: string) => TE.TaskEither<string, string[]>;
+type itemDetailById = (itemId: string) => TE.TaskEither<string, Item>;
+
+const fetchItemIdsByListRight = (
+  listId: string
+): TE.TaskEither<string, string[]> => TE.right(['1', '2', '3']);
+
+const fetchItemDetailByIdRight = (listId: string) => TE.left("There is no such list");
+
+const fetchItem = 
+  (fetchItemIdsByList:itemIdsByList, fetchItemDetailById: itemDetailById) => 
+  (listId: string): T.Task<string> =>
+    pipe(
+      listId,
+      E.fromNullable('The list is not presnet!'),
+      TE.fromEither,
+      TE.fold(
+        (left) => T.of(left),
+        (right) => T.of(`The items are ${JSON.stringify(right)}`)
+      )
+    );
+
+console.log(
+  fetchItem(fetchItemIdsByListRight, fetchItemDetailByIdRight)('1')());    
