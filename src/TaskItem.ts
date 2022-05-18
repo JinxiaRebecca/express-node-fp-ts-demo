bf924e3a14a92d4ade033153d454a7a5040aeda7
JinxiaRebecca <18502908857@163.com>
2022-05-18 09:32:06 +0800

    update TaskItem

---
+++ src/TaskItem.ts
@@ -0,0 +1,26 @@
+import { task } from "fp-ts";
+import { Either } from "fp-ts/lib/Either";
+import * as E from "fp-ts/lib/Either";
+
+export type TaskItem = {
+  id: number;
+  status?: string;
+};
+
+export var taskItems: TaskItem[] = [
+    { id: 1, status: "active" },
+    { id: 2, status: "suspended" },
+    { id: 3, status: "pending" },
+  ];
+
+export type TaskItemSpecification = {
+  minValue?: number;
+  checkIsExisted: boolean;
+};
+
+export const doubleIdValidation = (item: TaskItem) =>
+  E.fromPredicate(
+    (input: TaskItem) => !taskItems.some((taskItem) => input.id === taskItem.id),
+    () => "The item is already esixting"
+  )(item);
+
